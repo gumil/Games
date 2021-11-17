ed82deeb0260d73fe8e5febd17d3cb9e12d6740c
Miguel Panelo <miguelrpanelo@gmail.com>
2021-11-17 17:10:17 +0100

    Add detekt

--- app/src/main/kotlin/dev/gumil/games/ui/theme/Theme.kt
+++ app/src/main/kotlin/dev/gumil/games/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun GamesTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun GamesTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
