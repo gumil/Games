940d66a27f1c7e18111a16ba35990ba2e98cf297
Miguel Panelo <miguelrpanelo@gmail.com>
2021-11-19 08:24:22 +0100

    Add query for getting popular games

--- app/src/main/kotlin/dev/gumil/games/data/network/IgdbApi.kt
+++ app/src/main/kotlin/dev/gumil/games/data/network/IgdbApi.kt
@@ -3,6 +3,7 @@ package dev.gumil.games.data.network
 import dev.gumil.games.BuildConfig
 import dev.gumil.games.data.Game
 import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import retrofit2.http.Body
 import retrofit2.http.Headers
 import retrofit2.http.POST
@@ -14,4 +15,40 @@ interface IgdbApi {
     )
     @POST("v4/games")
     suspend fun getGames(@Body query: RequestBody): List<Game>
+
+    companion object {
+        fun queryPopularGames(offset: Int): RequestBody {
+            return """
+                fields
+                    name,
+                    total_rating,
+                    cover.image_id,
+                    platforms.name,
+                    platforms.platform_logo.image_id,
+                    first_release_date,
+                    game_modes.name,
+                    game_engines.name,
+                    genres.name,
+                    involved_companies.company.name,
+                    player_perspectives.name,
+                    storyline,
+                    summary,
+                    screenshots.image_id,
+                    themes.name,
+                    url,
+                    videos.name,
+                    videos.video_id;
+                where
+                    first_release_date != null &
+                    first_release_date < 1637161259 &
+                    total_rating > 70 &
+                    total_rating_count > 5 &
+                    parent_game = null;
+                sort first_release_date desc;
+                limit 10;
+                offset $offset;
+            """.trimIndent()
+                .toRequestBody()
+        }
+    }
 }
